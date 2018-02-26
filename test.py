69c24f8fb1acf0b58a59cacf0d4a5c251c240b95
root <root@docker.novalocal>
2018-02-26 06:08:18 +0000

    updated test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("test").getOrCreate()
 
+sc=spark.sparkContext
+
+rdd = sc.parallelize([1,2,3])
+
+print(rdd.collect())
 
 
 
